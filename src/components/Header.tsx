1d41003993c95fdeae5f78d4becc140e409810aa
Linda-404 <Linda-404@users.noreply.github.com>
2023-01-12 12:00:18 +0100

    install sass, add partials, add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+// import Logo from "../../public/images/badinfluencecoder_logo.svg";
+
+
+export default function Header() {
+    return (
+        <>
+        <h1></h1>
+        <h2></h2>
+        </>
+    )
+}
